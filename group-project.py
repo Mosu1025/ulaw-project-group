69085006715632ee2d8bcdc53ee0cc40df73689d
Mosu1025 <148863720+Mosu1025@users.noreply.github.com>
2023-10-25 12:15:29 +0100

    Add files via upload

---
+++ group-project.py
@@ -0,0 +1,9 @@
+#this is a group project with rachael, mohsan and salman
+# Declare variables for a simple arithmetic
+myClass = 2
+myHouse = 3
+print("this is the addition of myclass and myhouse")
+answer = myClass + myHouse
+print(answer)
+
+print("this is a simple addition using two values")
